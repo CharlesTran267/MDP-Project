26997a784ec8f7d955cf39413bbd555a87038645
CharlesTran267 <charlestran267@gmail.com>
2023-09-12 10:54:42 +0800

    initial image rec setup

---
+++ RPi/ImageRec/image_rec.py
@@ -0,0 +1,16 @@
+from roboflow import Roboflow
+import os
+import time
+
+# load model
+rf = Roboflow(api_key="3cR60WzeoK9LNrEVOyPT")
+project = rf.workspace().project("mdp-project")
+model = project.version(1).model
+
+for image in os.listdir("test_images"):
+    if image.endswith(".jpg"):
+        start = time.time()
+        print(model.predict(f"test_images/{image}", confidence=40, overlap=30).json())
+        print(f"Time taken: {time.time() - start}")
+
+
